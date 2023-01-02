fbe3af1c62d465f19bf4662c3a68a2089a916d51
DeveloperC <DeveloperC@protonmail.com>
2023-01-02 00:51:00 +0000

    refactor: quoting and bracing all variables

--- build-framework.sh
+++ build-framework.sh
@@ -5,7 +5,7 @@ command -v git >/dev/null 2>&1 || { echo >&2 "The 'git' command is not installed
 
 # Setup folder.
 INSTALLATION_DIR=`pwd`
-README_FILE="$INSTALLATION_DIR/README.md"
+README_FILE="${INSTALLATION_DIR}/README.md"
 START_BADGE="https://img.shields.io/badge/Upsteam%20Commit-"
 END_BADGE="-yellowgreen\)\]\("
 
@@ -15,16 +15,16 @@ if [ ! -f $README_FILE ]; then
 fi
 
 # Install zsh-autosuggestions
-AUTOSUGGESTIONS_DIRECTORY=$INSTALLATION_DIR/zsh-autosuggestions
+AUTOSUGGESTIONS_DIRECTORY="${INSTALLATION_DIR}/zsh-autosuggestions"
 AUTOSUGGESTIONS_URL="https://github.com/zsh-users/zsh-autosuggestions"
-rm -rf $AUTOSUGGESTIONS_DIRECTORY
-git clone $AUTOSUGGESTIONS_URL $AUTOSUGGESTIONS_DIRECTORY
+rm -rf "${AUTOSUGGESTIONS_DIRECTORY}"
+git clone "${AUTOSUGGESTIONS_URL}" "${AUTOSUGGESTIONS_DIRECTORY}"
 
 # Clean up zsh-autosuggestions
-if [ -d $AUTOSUGGESTIONS_DIRECTORY ]; then
-    cd $AUTOSUGGESTIONS_DIRECTORY
+if [ -d "${AUTOSUGGESTIONS_DIRECTORY}" ]; then
+    cd "${AUTOSUGGESTIONS_DIRECTORY}"
     GIT_HASH=`git rev-parse --short HEAD`
-    sed -i.bak -E "s|$START_BADGE[0-9a-fA-F]{7}$END_BADGE$AUTOSUGGESTIONS_URL|$START_BADGE$GIT_HASH$END_BADGE$AUTOSUGGESTIONS_URL|" $README_FILE
+    sed -i.bak -E "s|${START_BADGE}[0-9a-fA-F]{7}${END_BADGE}${AUTOSUGGESTIONS_URL}|${START_BADGE}${GIT_HASH}${END_BADGE}${AUTOSUGGESTIONS_URL}|" "${README_FILE}"
     rm -rf ./.git ./.circleci ./src ./spec ./.github
     rm ./.rubocop.yml ./Gemfile.lock ./Gemfile ./CHANGELOG.md ./DESCRIPTION ./VERSION ./URL ./.rspec ./ZSH_VERSIONS ./.editorconfig ./Makefile ./.ruby-version ./Dockerfile ./INSTALL.md ./LICENSE ./install_test_zsh.sh ./README.md ./zsh-autosuggestions.plugin.zsh
 else
@@ -33,16 +33,16 @@ else
 fi
 
 # Install zsh-syntax-highlighting
-HIGHLIGHTING_DIRECTORY=$INSTALLATION_DIR/zsh-syntax-highlighting
+HIGHLIGHTING_DIRECTORY="${INSTALLATION_DIR}/zsh-syntax-highlighting"
 HIGHLIGHTING_URL="https://github.com/zsh-users/zsh-syntax-highlighting"
-rm -rf $HIGHLIGHTING_DIRECTORY
-git clone $HIGHLIGHTING_URL $HIGHLIGHTING_DIRECTORY
+rm -rf "${HIGHLIGHTING_DIRECTORY}"
+git clone "${HIGHLIGHTING_URL}" "${HIGHLIGHTING_DIRECTORY}"
 
 # Clean up zsh-syntax-highlighting
-if [ -d $HIGHLIGHTING_DIRECTORY ]; then
-    cd $HIGHLIGHTING_DIRECTORY
+if [ -d "${HIGHLIGHTING_DIRECTORY}" ]; then
+    cd "${HIGHLIGHTING_DIRECTORY}"
     GIT_HASH=`git rev-parse --short HEAD`
-    sed -i.bak -E "s|$START_BADGE[0-9a-fA-F]{7}$END_BADGE$HIGHLIGHTING_URL|$START_BADGE$GIT_HASH$END_BADGE$HIGHLIGHTING_URL|" $README_FILE
+    sed -i.bak -E "s|${START_BADGE}[0-9a-fA-F]{7}${END_BADGE}${HIGHLIGHTING_URL}|${START_BADGE}${GIT_HASH}${END_BADGE}${HIGHLIGHTING_URL}|" "${README_FILE}"
     rm -rf ./.git ./docs ./tests ./images ./.github/
     rm ./COPYING.md ./changelog.md ./.gitignore ./HACKING.md ./Makefile ./.gitattributes ./INSTALL.md ./.version ./.revision-hash ./README.md ./release.md ./.editorconfig ./zsh-syntax-highlighting.plugin.zsh
     sed -i.bak -E '/typeset -g ZSH_HIGHLIGHT_VERSION*/d' ./zsh-syntax-highlighting.zsh
@@ -74,4 +74,4 @@ else
     exit 1
 fi
 
-rm $README_FILE.bak
+rm "${README_FILE}.bak"
