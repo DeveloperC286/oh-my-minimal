65543ed36bc51b9d77598c327ca35b511089bebb
DeveloperC <DeveloperC@protonmail.com>
2025-04-28 20:33:25 +0100

    build: cloning releases from GitHub (#66)
    * build: cloning releases from GitHub
    
    Closes #1 and #5.
    
    * feat: zsh-autosuggestions 0.7.1
    
    * feat: zsh-syntax-highlighting 0.8.0
    
    * feat: zsh-simple-abbreviations 1.0.0

--- zsh-simple-abbreviations/zsh-simple-abbreviations.zsh
+++ zsh-simple-abbreviations/zsh-simple-abbreviations.zsh
@@ -1,17 +1,20 @@
 #!/usr/bin/env zsh
 
-if [[ -n $ZSH_VERSION ]]; then
-		# Create new abbreviations map.
-		typeset -Ag _zsh_simple_abbreviations
+# Only run if executed in Zsh environment.
+if [[ -n ${ZSH_VERSION} ]]; then
+	# Create new abbreviations map.
+	typeset -Ag ZSH_SIMPLE_ABBREVIATIONS
 
-		fpath+=${0:A:h}/src
-		autoload -Uz zsh-simple-abbreviations
+	fpath+=${0:A:h}/src
+	autoload -Uz zsh-simple-abbreviations
 
-		# Create key binding on space to expand into a possible abbreviations.
-		zle -N __zsh_simple_abbreviations::expand
-		bindkey " "    __zsh_simple_abbreviations::expand
+	# Create key binding on space to expand into a possible abbreviations.
+	autoload -Uz __zsh_simple_abbreviations_expand
+	zle -N zsh_simple_abbreviations_expand __zsh_simple_abbreviations_expand
+	bindkey " " zsh_simple_abbreviations_expand
 
-		# Create key binding on control + space to insert a space without any possible abbreviations expansion.
-		zle -N __zsh_simple_abbreviations::insert_space
-		bindkey "^ "    __zsh_simple_abbreviations::insert_space
+	# Create key binding on control + space to insert a space without any possible abbreviations expansion.
+	autoload -Uz __zsh_simple_abbreviations_insert_space
+	zle -N zsh_simple_abbreviations_insert_space __zsh_simple_abbreviations_insert_space
+	bindkey "^ " zsh_simple_abbreviations_insert_space
 fi
